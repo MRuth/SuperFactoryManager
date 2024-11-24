92c262ceb4654ed5237868f28b29b4eee23644b7
TeamDman <TeamDman9201@gmail.com>
2024-11-24 17:00:43 -0500

    Enable performance tweaks

--- src/main/java/ca/teamdman/sfm/SFMPerformanceTweaks.java
+++ src/main/java/ca/teamdman/sfm/SFMPerformanceTweaks.java
@@ -1,11 +1,13 @@
 package ca.teamdman.sfm;
 
+import net.minecraftforge.fml.loading.FMLEnvironment;
+
 /**
  * Allows for disabling certain performance tweaks for analysis and debugging purposes
  */
 public class SFMPerformanceTweaks {
-    public static final boolean OBJECT_POOL_VALIDATION = false;
-    public static final boolean OBJECT_POOL_ENABLED = false;
-    public static final boolean REGEX_CACHE_ENABLED = false;
-    public static final boolean REGEX_PREDICATE_OPTIMIZATION = false;
+    public static final boolean OBJECT_POOL_VALIDATION = !FMLEnvironment.production;
+    public static final boolean OBJECT_POOL_ENABLED = true;
+    public static final boolean REGEX_CACHE_ENABLED = true;
+    public static final boolean REGEX_PREDICATE_OPTIMIZATION = true;
 }
