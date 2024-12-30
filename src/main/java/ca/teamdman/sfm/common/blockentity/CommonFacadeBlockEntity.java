b0a866437b5c728d104192a986907bb42aaf5a73
TeamDman <TeamDman9201@gmail.com>
2024-12-30 00:04:03 -0500

    undo remove redundant block update call lol

--- src/main/java/ca/teamdman/sfm/common/blockentity/CommonFacadeBlockEntity.java
+++ src/main/java/ca/teamdman/sfm/common/blockentity/CommonFacadeBlockEntity.java
@@ -6,6 +6,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
@@ -37,6 +38,10 @@ public abstract class CommonFacadeBlockEntity<T extends IFacadeBlockEntity.Facad
         if (newData.equals(facadeData)) return;
         this.facadeData = newData;
         setChanged();
+        if (level != null) {
+            BlockState state = getBlockState();
+            level.sendBlockUpdated(worldPosition, state, state, Block.UPDATE_IMMEDIATE);
+        }
         requestModelDataUpdate();
     }
 
